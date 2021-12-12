7e38e24ec791c406926379ee7746c5ecb46f01cc
Galina Popova <g89117301783@gmail.com>
2021-12-12 11:04:20 +0300

    Тестирование проекта: проверит код

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,15 @@
+"use strict";
+
 document.querySelector(`.burger-btn`).addEventListener(`click`, () => {
   document.querySelector(`.header-cover`).classList.toggle(`header--opened`)
 })
 
-
 // Фильтр
 
-document.querySelector(`.filter-btn__open`).addEventListener(`click`, () => {
-  document.querySelector(`.catalog-wrap`).classList.toggle(`filter--opened`)
-})
+// document.querySelector(`.filter-btn__open`).addEventListener(`click`, () => {
+//   document.querySelector(`.catalog-wrap`).classList.toggle(`filter--opened`)
+// })
 
-document.querySelector(`.filter__close`).addEventListener(`click`, () => {
-  document.querySelector(`.catalog-wrap`).classList.toggle(`filter--close`)
-})
+// document.querySelector(`.filter__close`).addEventListener(`click`, () => {
+//   document.querySelector(`.catalog-wrap`).classList.toggle(`filter--close`)
+// })
