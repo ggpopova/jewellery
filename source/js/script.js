f8694cc16323504f8bd90442f80bb7b0f45afacd
Galina Popova <g89117301783@gmail.com>
2021-12-13 22:10:22 +0300

    Тестирование проекта: проверка закончена

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,142 @@
 "use strict";
-
+// Menu burger
 document.querySelector(`.burger-btn`).addEventListener(`click`, () => {
   document.querySelector(`.header-cover`).classList.toggle(`header--opened`)
 })
 
-// Фильтр
+// Filter
+document.querySelector(`.filter-btn__open`)?.addEventListener(`click`, () => {
+  document.querySelector(`.catalog-wrap`).classList.add(`filter--opened`);
+})
 
-// document.querySelector(`.filter-btn__open`).addEventListener(`click`, () => {
-//   document.querySelector(`.catalog-wrap`).classList.toggle(`filter--opened`)
-// })
+document.querySelector(`.filter__close`)?.addEventListener(`click`, () => {
+  document.querySelector(`.catalog-wrap`).classList.remove(`filter--opened`);
+})
 
-// document.querySelector(`.filter__close`).addEventListener(`click`, () => {
-//   document.querySelector(`.catalog-wrap`).classList.toggle(`filter--close`)
-// })
+document.querySelector(`.login__close`).addEventListener(`click`, () => {
+  document.body.classList.toggle(`login--close`)
+})
+
+
+// localStorage
+const emailInput = document.querySelector(`#email`);
+
+const loginForm = document.querySelector(`.login__form`);
+
+
+if (localStorage.getItem(`email`)) {
+  emailInput.value = localStorage.getItem(`email`);
+}
+
+if (emailInput) {
+  emailInput.addEventListener(`change`, () => {
+    localStorage.setItem(`email`, emailInput.value);
+  });
+}
+
+
+//ESC
+document.addEventListener('keydown', function (e) {
+  if (event.keyCode == 27) {
+    window.close();
+  }
+});
+
+// Slider
+const DEFAULT_SLIDER_PAGE = 1;
+const DESKTOP_SLIDES_PER_PAGE = 4;
+const TABLET_SLIDES_PER_PAGE = 2;
+
+const sliderItems = document.querySelectorAll(`.slider__item`);
+const sliderButtonBwrd = document.querySelector(`.slider__button--bwrd`);
+const sliderButtonFwrd = document.querySelector(`.slider__button--frwd`);
+
+const overallDesktopSliderPages = sliderItems.length / DESKTOP_SLIDES_PER_PAGE;
+const overallTabletSliderPages = sliderItems.length / TABLET_SLIDES_PER_PAGE;
+
+let currentDesktopSliderPage = DEFAULT_SLIDER_PAGE;
+let currentTabletSliderPage = DEFAULT_SLIDER_PAGE;
+
+const sliderButtonBwrdClickHandler = () => {
+    if (window.innerWidth > 768) {
+        if (currentDesktopSliderPage === DEFAULT_SLIDER_PAGE) {
+            currentDesktopSliderPage = overallDesktopSliderPages;
+        } else {
+            currentDesktopSliderPage = currentDesktopSliderPage - 1;
+        }
+    } else {
+        if (currentTabletSliderPage === DEFAULT_SLIDER_PAGE) {
+            currentTabletSliderPage = overallTabletSliderPages;
+        } else {
+            currentTabletSliderPage = currentTabletSliderPage - 1;
+        }
+    }
+
+    const lastDesktopSlideShown = currentDesktopSliderPage * DESKTOP_SLIDES_PER_PAGE;
+    const firstDesktopSlideShown = lastDesktopSlideShown - DESKTOP_SLIDES_PER_PAGE;
+
+    const lastTabletSlideShown = currentTabletSliderPage * TABLET_SLIDES_PER_PAGE;
+    const firstTabletSlideShown = lastTabletSlideShown - TABLET_SLIDES_PER_PAGE;
+
+    sliderItems.forEach((slide, slideIndex) => {
+        if (window.innerWidth > 768) {
+            if (slideIndex >= firstDesktopSlideShown && slideIndex <= lastDesktopSlideShown - 1) {
+                slide.classList.remove(`slider__item--hidden`);
+            } else {
+                slide.classList.add(`slider__item--hidden`);
+            }
+        } else {
+            if (slideIndex >= firstTabletSlideShown && slideIndex <= lastTabletSlideShown - 1) {
+                slide.classList.remove(`slider__item--hidden`);
+            } else {
+                slide.classList.add(`slider__item--hidden`);
+            }
+        }
+    });
+};
+
+const sliderButtonFwrdClickHandler = () => {
+    console.log(window.innerWidth);
+
+    if (window.innerWidth > 768) {
+        if (currentDesktopSliderPage === overallDesktopSliderPages) {
+            currentDesktopSliderPage = DEFAULT_SLIDER_PAGE;
+        } else {
+            currentDesktopSliderPage = currentDesktopSliderPage + 1;
+        }
+    } else {
+        if (currentTabletSliderPage === overallTabletSliderPages) {
+            currentTabletSliderPage = DEFAULT_SLIDER_PAGE;
+        } else {
+            currentTabletSliderPage = currentTabletSliderPage + 1;
+        }
+    }
+
+    const lastDesktopSlideShown = currentDesktopSliderPage * DESKTOP_SLIDES_PER_PAGE;
+    const firstDesktopSlideShown = lastDesktopSlideShown - DESKTOP_SLIDES_PER_PAGE;
+
+    const lastTabletSlideShown = currentTabletSliderPage * TABLET_SLIDES_PER_PAGE;
+    const firstTabletSlideShown = lastTabletSlideShown - TABLET_SLIDES_PER_PAGE;
+
+    sliderItems.forEach((slide, slideIndex) => {
+        if (window.innerWidth > 768) {
+            if (slideIndex >= firstDesktopSlideShown && slideIndex <= lastDesktopSlideShown - 1) {
+                slide.classList.remove(`slider__item--hidden`);
+            } else {
+                slide.classList.add(`slider__item--hidden`);
+            }
+        } else {
+            if (slideIndex >= firstTabletSlideShown && slideIndex <= lastTabletSlideShown - 1) {
+                slide.classList.remove(`slider__item--hidden`);
+            } else {
+                slide.classList.add(`slider__item--hidden`);
+            }
+        }
+    });
+};
+
+sliderButtonBwrd?.addEventListener(`click`, sliderButtonBwrdClickHandler);
+
+sliderButtonFwrd?.addEventListener(`click`, sliderButtonFwrdClickHandler);
+
+console.log(sliderItems);
